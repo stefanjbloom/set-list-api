4c27e8ae4092d244086a6abaa08a02a1d6d2d6c0
Juliet Eyraud <juliet@turing.edu>
2024-07-15 14:34:52 -0500

    Add solutions

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,8 +1,7 @@
 require "rails_helper"
 
-RSpec.describe Artist, type: :model do
+RSpec.describe Artist do
   it {should have_many :songs}
-
 
   describe "instance methods" do
     describe "#average_song_length" do
@@ -26,6 +25,34 @@ RSpec.describe Artist, type: :model do
         artist.update(name: "The Artist Formerly Known as Prince", updated_at: updated_time)
 
         expect(artist.last_updated).to eq(updated_time.strftime("%Y-%m-%d"))
+      end
+    end
+
+    describe "#songs_alphabetically" do
+      before :each do
+        @prince = Artist.create!(name: "Prince")
+        @purple = @prince.songs.create!(title: "Purple Rain", length: 845, play_count: 8599)
+        @beret = @prince.songs.create!(title: "Raspberry Beret", length: 664, play_count: 99)
+        @other_song = @prince.songs.create!(title: "Another Prince Song", length: 1, play_count: 99)
+      end
+
+      it "Returns all of an artist's songs sorted by title alphabetically" do
+        expect(@prince.songs_alphabetically).to eq([@other_song, @purple, @beret])
+      end
+    end
+
+    describe "#shortest_songs(x)" do
+      before :each do
+        @prince = Artist.create!(name: "Prince")
+        @purple = @prince.songs.create!(title: "Purple Rain", length: 845, play_count: 8599)
+        @beret = @prince.songs.create!(title: "Raspberry Beret", length: 664, play_count: 99)
+        @other_song = @prince.songs.create!(title: "Another Prince Song", length: 1, play_count: 99)
+      end
+
+      it "Returns the x shortest songs for an artist, where x is an argument for the method" do
+        expect(@prince.shortest_songs(2)).to eq([@other_song, @beret])
+        expect(@prince.shortest_songs(3)).to eq([@other_song, @beret, @purple])
+        expect(@prince.shortest_songs(1)).to eq([@other_song])
       end
     end
   end
