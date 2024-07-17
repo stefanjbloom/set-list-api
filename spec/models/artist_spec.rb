b53c988c4169af6bd574da49197ce84187a13de3
Juliet Eyraud <juliet@turing.edu>
2024-07-17 14:55:30 -0500

    Update to include one to many with a few model methods

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -16,36 +16,5 @@ RSpec.describe Artist do
         expect(@prince.average_song_length.round(2)).to eq(503.33)
       end
     end
-
-    describe "#songs" do
-      it "returns all of the associated song objects" do
-        # First, Comment out the has_many :songs Association from the Artist model
-        # Then, make this test pass WITHOUT using Associations
-        expect(@prince.songs).to eq([@purple, @beret, @other_song])
-      end
-    end
-
-    describe "#song_count" do
-      it "counts the number of songs" do
-        # Make this test pass WITH Associations
-        expect(@prince.song_count).to eq(3)
-      end
-    end
-
-    describe "#wrote_song?" do
-      # Write two versions of this method
-      # Version 1 - Make these tests pass WITH Associations
-      # Version 2 - Make these tests pass WITHOUT Associations
-
-      it "returns true if the song is associated with the artist" do
-        expect(@prince.wrote_song?(@purple)).to be(true)
-      end
-
-      it "returns false if the song is not associated with the artist" do
-        diff_artist = Artist.create!(name: "Different Artist")
-        diff_song = diff_artist.songs.create!(title: "Different Song", length: 0, play_count: 0)
-        expect(@prince.wrote_song?(diff_song)).to be(false)
-      end
-    end
   end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -10,27 +10,5 @@ RSpec.describe Song, type: :model do
       @beret = Song.create!(title: "Raspberry Beret", length: 664, play_count: 99, artist_id: @prince.id)
       @other_song = Song.create!(title: "Another Prince Song", length: 1, play_count: 99, artist_id: @prince.id)
     end
-
-    describe "#artist" do
-      it "returns the associated artist object" do
-        # First, Comment out the belongs_to :artist Association from the Song model
-        # Then, make this test pass WITHOUT using Associations
-        expect(@purple.artist).to eq(@prince)
-      end
-    end
-
-    describe "#artist_name" do
-      it "returns the artists name" do
-        # Make this test pass WITH Associations
-        expect(@purple.artist_name).to eq("Prince")
-      end
-    end
-
-    describe "#other_artist_songs" do
-      it "returns the other songs by the artist and does not include itself" do
-        # Make this test pass WITH Associations
-        expect(@purple.other_artist_songs).to eq([@beret, @other_song])
-      end
-    end
   end
 end
