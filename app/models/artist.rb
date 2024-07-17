1ba0813989a6da142c28909de16bb31e84687c0b
Juliet Eyraud <juliet@turing.edu>
2024-07-17 10:27:39 -0500

    update model starting point for exercises

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,20 +2,7 @@ class Artist < ApplicationRecord
   has_many :songs
 
   def average_song_length
-    binding.pry
-    songs.average(:length)
+    self.songs.average(:length)
   end
 
-  def self.newest_first
-    Artist.order(created_at: :desc)
-  end
-
-  def last_updated
-    self.updated_at.strftime("%Y-%m-%d")
-  end
-
-  def self.by_name
-    binding.pry
-    order(:name)
-  end
 end
