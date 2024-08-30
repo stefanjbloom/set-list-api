37a931cee3fd3673e30520446cb0dc775fae7d1c
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2024-08-30 16:18:48 -0600

    Update song_serializer.rb

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,16 +1,4 @@
 class SongSerializer
-  def self.format_song(song)
-    {
-      id: song.id,
-      title: song.title,
-      length: song.length,
-      play_count: song.play_count
-    }
-  end
-
-  def self.format_songs(songs)
-    songs.each do |song|
-      format_song(song)
-    end
-  end
+  include JSONAPI::Serializer
+  attributes :title, :length
 end
